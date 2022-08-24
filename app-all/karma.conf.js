0587ccbde05d359c2765f4a94660cd5372287057
ArthurAraujo <arthurmoraes725@gmail.com>
2022-08-24 14:02:14 -0300

    a

--- app-all/karma.conf.js
+++ app-all/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/meu-primeiro-projeto'),
+      dir: require('path').join(__dirname, './coverage/app-all'),
       subdir: '.',
       reporters: [
         { type: 'html' },
