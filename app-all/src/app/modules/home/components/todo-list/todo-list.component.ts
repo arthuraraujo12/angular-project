65cae7b42cfd1e6b5ec1f8288dd6395f9ed44f73
ArthurAraujo <arthurmoraes725@gmail.com>
2022-08-31 11:04:33 -0300

    finished

--- app-all/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-all/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { first } from 'rxjs';
 
 // interface
 import { TaskList } from '../../model/task-list';
@@ -9,36 +10,50 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   // itens da lista
-  taskList: Array<TaskList> = [
-    // componetes de teste
-    // {task: "Sans Task", checked: true},
-    // {task: "outra Sans Task", checked: false}
-  ];
+  taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngDoCheck() {
+   this.setLocalStorate();
   }
 
-  setEmitTaskListSans(event: string){
+
+  setEmitTaskListSans(event: string) {
     // console.log(event);
-    this.taskList.push({task: event, checked: false});
+    this.taskList.push({ task: event, checked: false });
   }
 
   // deleta uma task
-  deleteItemTaskList(event: number){
+  deleteItemTaskList(event: number) {
     this.taskList.splice(event, 1);
   }
 
   // deleta todas task
-  deleteAllTaskList(){
+  deleteAllTaskList() {
     const confirm = window.confirm("Deleter todos Sans?");
-    if(confirm){
-    this.taskList = [];
+    if (confirm) {
+      this.taskList = [];
+    }
   }
-}
 
+  // deletea tesk vasias
+  inputValidation(event: string, index: number) {
+    if (!event.length) {
+      const confirm = window.confirm("TaskSans vazia, deseja deletar task da list?");
+      if (confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
+
+  setLocalStorate() {
+    if (this.taskList) {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
+    }
+  }
 }
